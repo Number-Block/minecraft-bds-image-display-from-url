4000f494cb3d6b7416e64380c51d2f2555110c79
Number-Block <126405195+Number-Block@users.noreply.github.com>
2023-05-19 21:56:54 +0900

    scriptでパーティクルをスポーンするようにしました

--- display_server/bin/display_server.dart
+++ display_server/bin/display_server.dart
@@ -1,5 +1,45 @@
+import 'dart:convert';
+import 'dart:io';
+
 import 'package:display_server/display_server.dart';
 
 void main() async {
-  startHttpServer();
+  Map<String, String> envVars = Platform.environment;
+
+  final host = InternetAddress.loopbackIPv4;
+  final port = int.parse(envVars['MINECRAFT_DISPLAY_PORT'] ?? '8080');
+
+  HttpServer server = await HttpServer.bind(host, port, shared: false);
+  await for (HttpRequest request in server) {
+    request.response.headers.contentType = ContentType.text;
+    if (request.method == 'POST' && request.uri.path == '/minecraft/display') {
+      print('POST request');
+      String url = await utf8.decodeStream(request);
+      if (url == "") {
+        request.response
+          ..statusCode = HttpStatus.notFound
+          ..write('')
+          ..close();
+        continue;
+      }
+      int width = int.parse(url.split(' ')[0]);
+      String extension = url.split(' ')[1];
+      if (!(extension == 'png' ||
+          extension == 'jpg' ||
+          extension == 'jpeg' ||
+          extension == 'ico')) continue;
+      await downloadImageFromURL(url.split(' ')[2], extension);
+      await pixelation(extension, width);
+      final colors = await getColors('result.$extension');
+      request.response
+        ..statusCode = HttpStatus.ok
+        ..write(colors)
+        ..close();
+    } else {
+      request.response
+        ..statusCode = HttpStatus.notFound
+        ..write('')
+        ..close();
+    }
+  }
 }
--- display_server/lib/display_server.dart
+++ display_server/lib/display_server.dart
@@ -1,4 +1,3 @@
-import 'dart:convert';
 import 'dart:io';
 import 'package:image/image.dart' as img;
 import 'package:http/http.dart';
@@ -16,8 +15,8 @@ Future pixelation(String extension, int width) async {
   return await cmd.executeThread();
 }
 
-Future<String> getCommand(String path) async {
-  String command = '';
+Future<String> getColors(String path) async {
+  String colors = '';
 
   final photo = await img.decodeImageFile(path);
   for (int y = 0; y < photo!.height; y++) {
@@ -32,53 +31,10 @@ Future<String> getCommand(String path) async {
       double g = colorSeparation(pixel.g);
       double b = colorSeparation(pixel.b);
 
-      command +=
-          'particle display:pixel_${r.toStringAsFixed(1)}_${g.toStringAsFixed(1)}_${b.toStringAsFixed(1)} ~${(x * 0.1).toStringAsFixed(2)} ~${(y * -0.1 + photo.height * 0.1).toStringAsFixed(2)} ~/';
+      colors +=
+          '${r.toStringAsFixed(8)},${g.toStringAsFixed(8)},${b.toStringAsFixed(8)},${(x * 0.1).toStringAsFixed(2)},${(y * -0.1 + photo.height * 0.1).toStringAsFixed(2)}/';
     }
   }
 
-  return command;
-}
-
-Map<String, String> envVars = Platform.environment;
-
-final host = InternetAddress.loopbackIPv4;
-final port = int.parse(envVars['MINECRAFT_DISPLAY_PORT'] ?? '8080');
-
-List<HttpRequest> getChatRequests = [];
-
-void startHttpServer() async {
-  HttpServer server = await HttpServer.bind(host, port, shared: false);
-  await for (HttpRequest request in server) {
-    request.response.headers.contentType = ContentType.text;
-    if (request.method == 'POST' && request.uri.path == '/minecraft/display') {
-      print('POST request');
-      String url = await utf8.decodeStream(request);
-      if (url == "") {
-        request.response
-          ..statusCode = HttpStatus.notFound
-          ..write('')
-          ..close();
-        continue;
-      }
-      int width = int.parse(url.split(' ')[0]);
-      String extension = url.split(' ')[1];
-      if (!(extension == 'png' ||
-          extension == 'jpg' ||
-          extension == 'jpeg' ||
-          extension == 'ico')) continue;
-      await downloadImageFromURL(url.split(' ')[2], extension);
-      await pixelation(extension, width);
-      final command = await getCommand('result.$extension');
-      request.response
-        ..statusCode = HttpStatus.ok
-        ..write(command)
-        ..close();
-    } else {
-      request.response
-        ..statusCode = HttpStatus.notFound
-        ..write('')
-        ..close();
-    }
-  }
+  return colors;
 }
